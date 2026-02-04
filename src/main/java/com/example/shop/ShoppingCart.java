413262750bcd8cedc3a5850694e98f94ad7f17f6
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-02-04 18:55:40 +0100

    feat: methods for adding items and calculating total price

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -2,6 +2,16 @@ package com.example.shop;
 
 public class ShoppingCart {
 
+    private int totalPrice = 0;
+
+    public void addItem(String name, int price) {
+        totalPrice += price;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
     public int getItemCount() {
         return 0;
     }
