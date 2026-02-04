417a340b007ea7c6160e0db03296150347f235a7
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-02-04 18:51:01 +0100

    test: adding one item updates total price

--- src/test/java/com/example/shop/ShoppingCartTest.java
+++ src/test/java/com/example/shop/ShoppingCartTest.java
@@ -16,4 +16,14 @@ class ShoppingCartTest {
         assertThat(itemCount).isZero();
     }
 
+    @Test
+    void addItem_shouldUpdateTotalPrice() {
+        // Arrange
+        ShoppingCart cart = new ShoppingCart();
+        // Act
+        cart.addItem("Keps", 200);
+        // Assert
+        assertThat(cart.getTotalPrice()).isEqualTo(200);
+    }
+
 }
