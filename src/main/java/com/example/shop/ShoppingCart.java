3b3598b0bb0b494a6e39476a701bf811c224c96a
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-02-04 20:01:03 +0100

    feat: add support for adding Product objects to ShoppingCart

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -1,12 +1,22 @@
 package com.example.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
 
     private double totalPrice = 0.0;
     private int itemCount = 0;
+    private final List<Product> items = new ArrayList<>();
 
     public void addItem(String name, double price) {
         totalPrice += price;
+        itemCount++;
+    }
+
+    public void addItem(Product product) {
+        items.add(product);
+        totalPrice += product.getPrice();
         itemCount++;
     }
 
