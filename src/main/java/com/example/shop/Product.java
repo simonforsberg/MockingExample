2b1336c613d432d8add3bd1d601f6a5dc049afeb
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-02-05 20:16:54 +0100

    docs: add Javadocs to Product class

--- src/main/java/com/example/shop/Product.java
+++ src/main/java/com/example/shop/Product.java
@@ -2,6 +2,12 @@ package com.example.shop;
 
 import java.util.Objects;
 
+/**
+ * Immutable value object representing a product.
+ * <p>
+ * Products are uniquely identified by name and price
+ * and can be used as keys in collections.
+ */
 public class Product {
     private final String name;
     private final double price;
